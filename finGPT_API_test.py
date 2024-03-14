332152a0031cd600e9904a322250cd0d96fc8842
YSW2 <jrgyber@gmail.com>
2024-03-14 18:40:16 +0900

    fin api test

--- finGPT_API_test.py
+++ finGPT_API_test.py
@@ -1,5 +1,7 @@
 from gradio_client import Client
+import time
 
+start_time = time.time()
 client = Client("https://fingpt-fingpt-forecaster.hf.space/--replicas/bbyl6/")
 result = client.predict(
     "AAPL",  # str  in 'Ticker' Textbox component
@@ -8,4 +10,8 @@ result = client.predict(
     False,  # bool  in 'Use Latest Basic Financials' Checkbox component
     api_name="/predict",
 )
+end_time = time.time()
+
+time_taken = end_time - start_time
+print(f"Time taken: {time_taken:.2f} seconds")
 print(result)
