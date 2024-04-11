9e13e528979cc1c6ebb698d4f07665a937eb40b6
MoonGyubeen's MacBook <moongyubeensmacbook@MoonGyubeenss-MacBook-Air.local>
2024-04-11 20:15:04 +0900

    feat: 프로젝트 등록, 로그인 기능(인증x)

---
+++ eta/src/main/java/com/example/eta/service/UserService.java
@@ -0,0 +1,66 @@
+package com.example.eta.service;
+
+import com.example.eta.dto.UserDto;
+import com.example.eta.entity.User;
+import com.example.eta.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+    @Autowired
+    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+//        this.passwordEncoder = passwordEncoder;
+    }
+
+
+
+    @Transactional //변경
+    public Long join(User user) {
+        userRepository.save(user);
+        return user.getUserId();
+    }
+
+    @Transactional // 데이터 변경
+    public void update(Long id, UserDto userDto) {
+        // User 엔티티 조회
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다. id=" + id));
+
+        //업데이트
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+        //userRepository.save(user); 해줄필요없음 더티체킹으로 반영
+    }
+
+    public User findOne(Long id) {
+        // ID로 User 엔티티 조회
+        return userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다. id=" + id));
+    }
+
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
+
+    public User registerNewUser(UserDto userDto) {
+        // User 엔티티 생성
+        User user = new User();
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+
+        // User 저장
+        return userRepository.save(user);
+    }
+}
