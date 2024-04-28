28e454ed5383b76d7585b2aa76d5ba39751ed142
Yongsoo98 <dydtn02@gmail.com>
2024-04-28 12:30:06 +0900

    로그인 화면 추가

--- Components/App.js
+++ Components/App.js
@@ -1,14 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Home from "./Home";
+
 import ViewPortfolio from "./ViewPortfolio";
 import PortfolioDetails from "./PortfolioDetails";
+import Login from './screens/login';
+import Signup from './screens/signup';
+import Home from './Home';
 
 const Stack = createStackNavigator();
 
 function ScreenStack() {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="ViewPortfolio" component={ViewPortfolio} />
       <Stack.Screen name="PortfolioDetails" component={PortfolioDetails} />
---
+++ Components/screens/login.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Button,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const Login = () => {
+  const navigation = useNavigation();
+
+  const [useremail, setUseremail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.HomeText}>로그인 화면</Text>
+      
+      <TextInput
+        style={styles.inputbox}
+        value={useremail}
+        placeholder="Email"
+        onChangeText={setUseremail}
+      ></TextInput>
+      <TextInput
+        style={styles.inputbox}
+        value={password}
+        placeholder="Password"
+        onChangeText={setPassword}
+      ></TextInput>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Home", { screen: 'Home' })}
+        style={styles.Inputbotton}
+      >
+        <Text style={styles.BottomText}>로그인</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        //onPress={() => navigation.navigate("SocialLogin", { screen: 'SocialLogin' })}
+        style={styles.Inputbotton}
+      >
+        <Text style={styles.BottomText}>소셜 로그인</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Signup", { screen: 'Signup' })}
+        style={styles.NextBottom}
+      >
+        <Text style={styles.BottomText}>회원가입</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: '#fff',
+  },
+  HomeText: {
+    fontSize: 30,
+    textAlign: "center",
+    marginBottom: "10%",
+  },
+  NextBottom: {
+    backgroundColor: "purple",
+    padding: 10,
+    marginTop: "10%",
+    width: "50%",
+    alignSelf: "center",
+    borderRadius: 10,
+  },
+  BottomText: {
+    fontSize: 15,
+    color: 'white',
+    textAlign: "center",
+  },
+  Inputbotton: {
+    backgroundColor: "purple",
+    width: "50%",
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 10
+  },
+  inputbox: {
+    width: "50%",
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    marginBottom: 10
+  }
+})
