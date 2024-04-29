bb3014e644bffbe850eb8cbabf4e8080228bf8aa
Yongsoo98 <dydtn02@gmail.com>
2024-04-29 19:02:16 +0900

    merge frontend and lys

--- Components/App.js
+++ Components/App.js
@@ -1,11 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import ViewPortfolio from "./ViewPortfolio";
-import PortfolioDetails from "./PortfolioDetails";
 import Login from './screens/login';
+import SocialLogin from './screens/sociallogin';
 import Signup from './screens/signup';
-import Home from './Home';
+
+import Home from "./pages/Home";
+import ViewPortfolio from "./pages/ViewPortfolio";
+import PortfolioDetails from "./pages/PortfolioDetails";
+import MakePortfolio from "./pages/MakePortfolio";
 
 const Stack = createStackNavigator();
 
@@ -13,10 +16,12 @@ function ScreenStack() {
   return (
     <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="SocialLogin" component={SocialLogin} />
       <Stack.Screen name="Signup" component={Signup} />
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="ViewPortfolio" component={ViewPortfolio} />
       <Stack.Screen name="PortfolioDetails" component={PortfolioDetails} />
+      <Stack.Screen name="MakePortfolio" component={MakePortfolio} />
     </Stack.Navigator>
   );
 }
---
+++ Components/utils/urls.js
@@ -0,0 +1,5 @@
+const urls = {
+  fastapiUrl: "https://192.168.220.136:8000",
+};
+
+export default urls;
