6cb7a405b6a1c3cae551925de7eaec57f2283e6d
Sungcheol Heo <suprlux09@gmail.com>
2024-05-07 20:21:42 +0900

    feat: 자동포트폴리오 리밸런싱 대상인지 판별하는 코드 작성

--- eta/src/main/java/com/example/eta/scheduler/PortfolioScheduler.java
+++ eta/src/main/java/com/example/eta/scheduler/PortfolioScheduler.java
@@ -20,31 +20,81 @@ public class PortfolioScheduler {
     private final PriceRepository priceRepository;
 
     @Scheduled(cron = "0 0 0 * * 1-5")
-    public void createRebalancingNotification() {
+    public void doProportionRebalancing() {
         for (Portfolio portfolio : portfolioRepository.findAllByIsAutoIsTrue()) {
             updateProportion(portfolio);
+            if(isProportionRebalancingNeeded(portfolio))
+                createProportionRebalancing(portfolio);
         }
     }
 
     public void updateProportion(Portfolio portfolio) {
-        float totalValue = portfolio.getCurrentCash();
-        Map<PortfolioTicker, Double> closeMulNumForTicker = new HashMap<>();
+        float totalAmount = portfolio.getCurrentCash();
+        Map<PortfolioTicker, Float> currentAmountForTicker = new HashMap<>();
 
         // 1. 종가를 가져와서 각 종목별로 종가*개수 계산
         // 2. 전체 종가*개수 + 현금 계산
         for (PortfolioTicker portfolioTicker : portfolio.getPortfolioTickers()) {
             float number = portfolioTicker.getNumber();
-            double close = priceRepository.findLatestPriceByTicker(portfolioTicker.getTicker().getTicker())
-                    .get().getClose();
+            float close = priceRepository.findLatestPriceByTicker(portfolioTicker.getTicker().getTicker())
+                    .get().getClose().floatValue();
 
-            totalValue += close*number;
-            closeMulNumForTicker.put(portfolioTicker, close*number);
+            totalAmount += close*number;
+            currentAmountForTicker.put(portfolioTicker, close*number);
         }
 
         // 3. 1, 2에서 계산한 값 가지고 각 종목별로 비중 계산하고 업데이트
-        for (PortfolioTicker portfolioTicker : closeMulNumForTicker.keySet()) {
-            float currentProportion = closeMulNumForTicker.get(portfolioTicker).floatValue() / totalValue;
+        for (PortfolioTicker portfolioTicker : currentAmountForTicker.keySet()) {
+            float currentProportion = currentAmountForTicker.get(portfolioTicker).floatValue() / totalAmount;
             portfolioTicker.setCurrentProportion(currentProportion);
         }
+    }
+
+    public boolean isProportionRebalancingNeeded(Portfolio portfolio) {
+        // 기존 비중 대비 현재 비중의 차이가 0.2% 이상 나는 경우
+        return portfolio.getPortfolioTickers().stream().anyMatch(portfolioTicker -> {
+            float initProportion = portfolioTicker.getInitProportion();
+            float currentProportion = portfolioTicker.getCurrentProportion();
+            return currentProportion / initProportion < 0.8 || currentProportion / initProportion > 1.2;
+        });
+    }
+
+    
+    public void createProportionRebalancing(Portfolio portfolio) {
+        Map<PortfolioTicker, Float> currentAmountForTicker = new HashMap<>();
+        Map<PortfolioTicker, Float> targetAmountForTicker = new HashMap<>();
+
+        // 현재 총자산 계산하고, 각 종목별 비중에 따라 목표 보유량(총 가격) 계산
+        float totalAmount = portfolio.getCurrentCash();
+        for (PortfolioTicker portfolioTicker : portfolio.getPortfolioTickers()) {
+            float number = portfolioTicker.getNumber();
+            float close = priceRepository.findLatestPriceByTicker(portfolioTicker.getTicker().getTicker())
+                    .get().getClose().floatValue();
+
+            totalAmount += close*number;
+            currentAmountForTicker.put(portfolioTicker, close*number);
+        } 
+       
+        for (PortfolioTicker portfolioTicker : portfolio.getPortfolioTickers()) {
+            float targetAmount = totalAmount * portfolioTicker.getInitProportion();
+            targetAmountForTicker.put(portfolioTicker, targetAmount);
+        }
+        
+        // 매도, 매수 알림 생성
+        for (PortfolioTicker portfolioTicker : portfolio.getPortfolioTickers()) {
+            float close = priceRepository.findLatestPriceByTicker(portfolioTicker.getTicker().getTicker())
+                    .get().getClose().floatValue();
+            float targetAmount = targetAmountForTicker.get(portfolioTicker);
+            float currentAmount = currentAmountForTicker.get(portfolioTicker);
+            float diff = targetAmount - currentAmount;
+            if (diff > 0) {
+                // 매수
+                
+            } else if (diff < 0) {
+                // 매도
+                
+            }
+
+        } 
     }
 }
