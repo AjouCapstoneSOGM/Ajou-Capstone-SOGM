8204af5d9e84ed76c5044718b425b89a6f4d9d4c
somea82 <joemoon0526@gmail.com>
2024-05-13 02:42:52 +0900

    feat: 5.12 완료상황(new관련 Api미완성)

---
+++ eta/src/main/java/com/example/eta/controller/NewsController.java
@@ -0,0 +1,33 @@
+package com.example.eta.controller;
+
+import com.example.eta.dto.NewsDto;
+import com.example.eta.service.NewsService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/stocks")
+public class NewsController {
+    private final NewsService newsService;
+
+//    @Autowired
+//    public NewsController(NewsService newsService) {
+//        this.newsService = newsService;
+//    }
+//
+//    @GetMapping("/{ticker}/news")
+//    public ResponseEntity<NewsDto> getNews(@PathVariable{"ticker"} String ticker) {
+//        NewsDto news = newsService.getLatestNews(ticker);
+//        if (news != null) {
+//            return ResponseEntity.ok(news);
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+}
--- eta/src/main/java/com/example/eta/service/RebalancingService.java
+++ eta/src/main/java/com/example/eta/service/RebalancingService.java
@@ -1,26 +1,28 @@
 package com.example.eta.service;
 
+import com.example.eta.dto.RebalancingDto;
 import com.example.eta.entity.RebalancingTicker;
+import com.example.eta.entity.Ticker;
 import com.example.eta.repository.RebalancingRepository;
+import com.example.eta.repository.TickerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RebalancingService {
-    RebalancingRepository rebalancingRepository;
+    private final TickerRepository tickerRepository;
+    private final RebalancingRepository rebalancingRepository;
 
     // 포트폴리오 ID로 리밸런싱 알림 존재 여부 확인
     public boolean existsRebalancingByPortfolioId(Integer pfId) {
         return rebalancingRepository.existsByPortfolioPfId(pfId);
     }
 
-    // 포트폴리오 ID로 모든 리밸런싱 알림 받아오기
-    public List<RebalancingTicker> findAllRebalancingByPortfolioId(Integer pfId) {
-        return rebalancingRepository.findByPortfolioPfId(pfId);
-    }
     // 알림 ID로 리밸린성 알림 삭제
     public void deleteRebalancing(Integer rnId) throws Exception {
         if (!rebalancingRepository.existsById(rnId)) {
@@ -29,5 +31,19 @@ public class RebalancingService {
         rebalancingRepository.deleteById(rnId);
     }
 
-
+    // 포트폴리오 ID로 모든 리밸런싱 알림 받아오기
+    public List<RebalancingDto.InfoDto> getAllRebalancingsByPortfolioId(Integer pfId) {
+        List<RebalancingDto.InfoDto> rebalancings = new ArrayList<>();
+        List<RebalancingTicker> rebalancingTickers = rebalancingRepository.findByPortfolioPfId(pfId);
+        for (RebalancingTicker rt : rebalancingTickers) {
+            Optional<Ticker> ticker = tickerRepository.findById(rt.getTicker().getTicker());
+            ticker.ifPresent(value -> rebalancings.add(new RebalancingDto.InfoDto(
+                    value.getTicker(),
+                    value.getName(),
+                    rt.getNumber(),
+                    rt.getIsBuy()
+            )));
+        }
+        return rebalancings;
+    }
 }
