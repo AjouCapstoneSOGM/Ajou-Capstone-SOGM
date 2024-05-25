c7c9c72a8fe09d64248d1c393abacd872a5ea744
YSW2 <jrgyber@gmail.com>
2024-05-26 05:35:21 +0900

    포트폴리오 자동 제작 1, 2페이지 스타일링

--- Components/pages/make/auto/AutoPage1.js
+++ Components/pages/make/auto/AutoPage1.js
@@ -1,69 +1,118 @@
 import React from "react";
 import { View, TextInput, TouchableOpacity, StyleSheet } from "react-native";
-
 import AppText from "../../../utils/AppText";
 import { filteringNumber } from "../../../utils/utils";
 
 const AutoPage1 = ({ amount, setAmount }) => {
-  const isAmountEnough = () => {
-    if (amount >= 1000000) {
-      return true;
-    }
-    return false;
-  };
+  const isAmountEnough = () => amount >= 1000000;
 
   const handleAmount = (value) => {
-    setAmount(filteringNumber(value));
+    const filteredValue = filteringNumber(value);
+    setAmount(filteredValue);
   };
 
   const addValuetoAmount = (value) => {
-    setAmount(Number(amount) + value);
+    setAmount((prevAmount) => Number(prevAmount) + value);
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.textContainer}>
-        <AppText style={{ fontSize: 25 }}>
-          포트폴리오에 사용할 금액을 입력해주세요.
+        <AppText style={styles.titleText}>
+          얼마를 투자하실 생각이신가요?
         </AppText>
       </View>
-      <View style={styles.inputContainer}>
+      <View style={styles.contentsContainer}>
         <TextInput
-          style={styles.input_Amount}
+          style={styles.inputAmount}
           keyboardType="numeric"
           value={amount.toString()}
-          onChangeText={(value) => Number(handleAmount(value))}
-          placeholder="금액을 입력하세요"
+          onChangeText={handleAmount}
+          placeholder="금액을 입력해주세요"
+          placeholderTextColor="#808080"
         />
-        <View style={{ flexDirection: "row", marginBottom: 10 }}>
+        <View style={styles.buttonRow}>
           <TouchableOpacity
             style={styles.addButton}
             onPress={() => addValuetoAmount(100000)}
           >
-            <AppText style={{ color: "#555" }}>+ 10만</AppText>
+            <AppText style={styles.buttonText}>+10만</AppText>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.addButton}
             onPress={() => addValuetoAmount(1000000)}
           >
-            <AppText style={{ color: "#555" }}>+ 100만</AppText>
+            <AppText style={styles.buttonText}>+100만</AppText>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.addButton}
             onPress={() => addValuetoAmount(10000000)}
           >
-            <AppText style={{ color: "#555" }}>+ 1000만</AppText>
+            <AppText style={styles.buttonText}>+1000만</AppText>
           </TouchableOpacity>
         </View>
-        {amount && !isAmountEnough() && (
-          <AppText style={{ color: "red", alignSelf: "baseline" }}>
+        {amount > 0 && !isAmountEnough() && (
+          <AppText style={styles.errorText}>
             최소 100만원 이상 가능합니다.
           </AppText>
         )}
       </View>
-      <View style={styles.buttonContainer}></View>
     </View>
   );
 };
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentsContainer: {
+    height: "100%",
+    backgroundColor: "#333",
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+  },
+  textContainer: {
+    paddingHorizontal: 10,
+    paddingBottom: 20,
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  inputAmount: {
+    backgroundColor: "transparent",
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+    paddingVertical: 5,
+    marginTop: 30,
+    fontSize: 30,
+    color: "#f0f0f0",
+  },
+  buttonRow: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    marginTop: 20,
+  },
+  addButton: {
+    backgroundColor: "transparent",
+    paddingRight: 20,
+  },
+  buttonText: {
+    color: "#f0f0f0",
+    fontWeight: "bold",
+  },
+  errorText: {
+    fontSize: 15,
+    marginTop: 20,
+    color: "#ff5858",
+  },
+  nextButtonContainer: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    right: 20,
+  },
+});
+
 export default AutoPage1;
