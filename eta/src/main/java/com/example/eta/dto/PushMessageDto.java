9e733ea5f73b1b1c8f7b768ab66d4b00b10d59ec
Sungcheol Heo <suprlux09@gmail.com>
2024-05-25 14:23:31 +0900

    feat: 리밸런싱 알림 생성 시 푸시 알림 요청
    푸시 알림 형식:
    리밸런싱 알림
    {name} 포트폴리오에 리밸런싱 해야 할 종목이 있어요!
    
    {이름}{티커} {개수}개 {매수/매도}

--- eta/src/main/java/com/example/eta/dto/PushMessageDto.java
+++ eta/src/main/java/com/example/eta/dto/PushMessageDto.java
@@ -4,11 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Map;
+
 @Builder
 @AllArgsConstructor
 @Getter
 public class PushMessageDto {
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class PushMessageData {
+        private int pfId;
+        private int rnId;
+    }
+
     private String to;
     private String title;
     private String body;
+    private PushMessageData data;
 }
+
