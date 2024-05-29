85e214840d1448d76e95a63cfad33f2376c1cc13
LeeYongsoo <dydtn02@ajou.ac.kr>
2024-05-29 19:04:08 +0900

    fix: signup

--- Components/pages/login/signup.js
+++ Components/pages/login/signup.js
@@ -1,11 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  TextInput,
-  Alert,
-} from "react-native";
+import { View, StyleSheet, TouchableOpacity, Alert, TextInput } from "react-native";
 import urls from "../../utils/urls";
 import AppText from "../../utils/AppText";
 import { width, height } from "../../utils/utils";
@@ -13,69 +7,135 @@ import { width, height } from "../../utils/utils";
 const Signup = ({ navigation }) => {
   const [username, setUsername] = useState("");
   const [useremail, setUseremail] = useState("");
-  const [verifyNum, setVerifynum] = useState(""); // api 설정 필요
   const [password, setPassword] = useState("");
   const [pwcheck, setpwcheck] = useState("");
 
   const [checkEmail, setCheckEmail] = useState("");
-  const [checkNum, setChecknum] = useState("");
   const [checkPw, setCheckPw] = useState("");
   const [checkPwLen, setCheckPwLen] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
+  const [isEmailVerified, setIsEmailVerified] = useState(false);
+  const [signupToken, setSignupToken] = useState("");
 
   const MinPasswordLength = 10;
 
   const fetchSignupInfo = async () => {
-    fetch(`${urls.springUrl}/api/auth/signup`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: useremail,
-        password: password,
-        name: username,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
+    try{
+      console.log(useremail, password, username, signupToken);
+      const response = await fetch(`${urls.springUrl}/api/auth/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: useremail,
+          password: password,
+          name: username,
+          signupToken: signupToken,
+        }),
       })
-      .catch((error) => {
+      const data = await response.json();
+      return data.status;
+    }catch(error) {
         console.error("Error:", error);
-      });
+    };
   };
+
+  const sendVerificationCode = async () => {
+    try{
+      const response = await fetch(`${urls.springUrl}/api/auth/send-verification-code`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: useremail,
+        }),
+      })
+      if(response.status == 200){
+        Alert.alert("인증 코드 발송", "인증 코드가 이메일로 발송되었습니다.");
+      } else if (response.status == 409){
+        Alert.alert("이미 가입된 이메일입니다.");
+      }
+      else{
+        Alert.alert("잠시 후 다시 시도해 주세요.");
+      }
+    } catch(error) {
+        console.error("Error:", error);
+      };
+  };
+
+  const verifyCode = async () => {
+    try{
+      const response =  await fetch(`${urls.springUrl}/api/auth/verify-email`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: useremail,
+          code: verificationCode,
+        }),
+      });
+      if (response.ok){
+        const data = await response.json();
+        setIsEmailVerified(true);
+        setSignupToken(data.signupToken);
+        Alert.alert("인증 성공", "이메일 인증이 완료되었습니다.");
+      }
+      else if (response.status == 401){
+        Alert.alert("인증 실패", "인증번호가 만료되었습니다.");
+      }
+      else if (response.status == 403){
+        Alert.alert("인증 실패","인증번호가 일치하지 않습니다.");
+      }
+      else if (response.status == 404){
+        Alert.alert("인증 실패","인증하기 버튼을 눌러 이메일 인증을 진행해 주세요.");
+      }
+      else{
+        Alert.alert("인증 실패","잠시 후 다시 시도해 주세요.");
+      }
+    } catch(error) {
+        console.error("Error:", error);
+    };
+  };
+
   const isEmailValid = (email) => {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return regex.test(email);
   };
 
   const isValueValid = () => {
-    if (username && checkEmail && checkPw && checkPwLen) return true;
+    if (username && checkEmail && checkPw && checkPwLen && isEmailVerified) return true;
     return false;
   };
 
   const handleSignUp = async () => {
     if (isValueValid()) {
-      await fetchSignupInfo();
-      Alert.alert("회원가입 완료", "회원가입이 완료되었습니다.", [
+      const respone = await fetchSignupInfo();
+      if (respone == 409) {
+        //Alert.alert("이미 가입된 이메일입니다.")
+      }
+      else if(respone == 404) {
+        Alert.alert("이메일 인증을 진행해 주세요.")
+      }
+      else if(respone == 403) {
+        Alert.alert("인증번호가 잘못되었습니다.")
+      }
+      else{
+        Alert.alert("회원가입 완료", "회원가입이 완료되었습니다.", [
         {
           text: "확인",
           onPress: () => {
-            navigation.navigate("Login");
+            navigation.goBack();
           },
           style: "destructive",
         },
       ]);
-    }
-  };
 
-  const handleSendverify = () => {
-    // 버튼이 눌렸을 때 처리할 로직을 작성합니다.
-    console.log("Button pressed");
-  };
-  const handleCenkverify = () => {
-    // 버튼이 눌렸을 때 처리할 로직을 작성합니다.
-    console.log("Button pressed");
+      }
+      
+    }
   };
 
   useEffect(() => {
@@ -101,32 +161,30 @@ const Signup = ({ navigation }) => {
       else setCheckPw(true);
     }
   }, [pwcheck]);
+
   return (
     <View style={styles.container}>
       <AppText style={styles.HomeText}>회원가입</AppText>
-      <View style={styles.allContainer}>
-        <View style={styles.inputContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputBox}
+          value={username}
+          placeholder="이름"
+          onChangeText={setUsername}
+        />
+        <View style={styles.emailContainer}>
           <TextInput
-            style={styles.inputBox}
-            value={username}
-            placeholder="닉네임"
-            onChangeText={setUsername}
-            placeholderTextColor="grey"
-          ></TextInput>
-        </View>
-        <View style={styles.verifyInputContainer}>
-          <TextInput
-            style={styles.verifyInputBox}
-            value={useremail}
             onChangeText={setUseremail}
+            value={useremail}
             placeholder="이메일"
-            placeholderTextColor="grey"
+            style={[styles.inputBox, { flex: 1 }]}
           />
           <TouchableOpacity
+            onPress={sendVerificationCode}
             style={styles.verifyButton}
-            onPress={handleSendverify}
+            disabled={!isEmailValid(useremail)}
           >
-            <AppText style={styles.verifybuttonText}>인증번호 전송</AppText>
+            <AppText style={styles.verifyButtonText}>인증하기</AppText>
           </TouchableOpacity>
         </View>
         {!checkEmail && useremail && (
@@ -134,59 +192,40 @@ const Signup = ({ navigation }) => {
             올바른 형식을 입력해주세요
           </AppText>
         )}
-        <View style={styles.verifyInputContainer}>
-          <TextInput
-            value={checkNum}
-            onChangeText={setChecknum}
-            placeholder="인증번호"
-            style={styles.verifyInputBox}
-            placeholderTextColor="grey"
-            keyboardType="number-pad"
-          ></TextInput>
-          <TouchableOpacity
-            style={styles.verifyButton}
-            onPress={handleCenkverify}
-          >
-            <AppText style={styles.verifybuttonText}>인증번호 확인</AppText>
-          </TouchableOpacity>
-          {!checkNum && verifyNum && (
-            <AppText style={styles.notificationText}>
-              인증번호가 틀렸습니다.
-            </AppText> // api 필요
-          )}
-        </View>
-        <View style={styles.inputContainer}>
-          <TextInput
-            onChangeText={setPassword}
-            value={password}
-            placeholder="비밀번호"
-            style={styles.inputBox}
-            secureTextEntry
-            placeholderTextColor="grey"
-          ></TextInput>
-          {!checkPwLen && password && (
-            <AppText style={styles.notificationText}>
-              비밀번호는 {MinPasswordLength}자리 이상이어야 합니다
-            </AppText>
-          )}
-        </View>
-        <View style={styles.inputContainer}>
-          <TextInput
-            onChangeText={setpwcheck}
-            value={pwcheck}
-            placeholder="비밀번호 확인"
-            style={styles.inputBox}
-            secureTextEntry
-            placeholderTextColor="grey"
-          ></TextInput>
-          {!checkPw && pwcheck && (
-            <AppText style={styles.notificationText}>
-              비밀번호를 정확히 입력해주세요
-            </AppText>
-          )}
-        </View>
+        <TextInput
+          onChangeText={setPassword}
+          value={password}
+          placeholder="비밀번호"
+          style={styles.inputBox}
+          secureTextEntry
+        />
+        {!checkPwLen && password && (
+          <AppText style={styles.notificationText}>
+            비밀번호는 {MinPasswordLength}자리 이상이어야 합니다
+          </AppText>
+        )}
+        <TextInput
+          onChangeText={setpwcheck}
+          value={pwcheck}
+          placeholder="비밀번호 확인"
+          style={styles.inputBox}
+          secureTextEntry
+        />
+        {!checkPw && pwcheck && (
+          <AppText style={styles.notificationText}>
+            비밀번호를 정확히 입력해주세요
+          </AppText>
+        )}
+        <TextInput
+          onChangeText={setVerificationCode}
+          value={verificationCode}
+          placeholder="인증 코드"
+          style={styles.inputBox}
+        />
+        <TouchableOpacity onPress={verifyCode} style={styles.verifyButton}>
+          <AppText style={styles.verifyButtonText}>인증 코드 확인</AppText>
+        </TouchableOpacity>
       </View>
-
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           onPress={handleSignUp}
