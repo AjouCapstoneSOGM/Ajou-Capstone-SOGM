4dc0e3430f2476925578aea2264fa2a896f76982
somea82 <joemoon0526@gmail.com>
2024-06-02 23:36:51 +0900

    fect: 유저관련 조회기능(ID제외) 완료

--- src/main/java/com/example/adminpage/controller/UserController.java
+++ src/main/java/com/example/adminpage/controller/UserController.java
@@ -5,15 +5,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public String getUsers(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "Users";
+    }
+    @GetMapping("/search/{searchType}/{query}")
+    public String search(@PathVariable("searchType") String searchType, @PathVariable("query") String query, Model model) {
+        System.out.println("Search Type: " + searchType);
+        System.out.println("Query: " + query);
+        switch (searchType) {
+            case "id":
+                userRepository.findByUserId(Integer.valueOf(query)).ifPresent(user -> model.addAttribute("users", List.of(user)));
+            case "name":
+                model.addAttribute("users", userRepository.findAllByNameContaining(query));
+                break;
+            default:
+        }
+        return "users";
     }
 }
