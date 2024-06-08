b60a76bffea2f021bcc0d03080e6a5af8f5daf7d
Yongsoo98 <dydtn02@gmail.com>
2024-06-09 06:38:19 +0900

    add: 수동 포트폴리오 종목 추가에서 가치지표 상위권 종목에서 추가하기

---
+++ Components/pages/make/manual/Page1.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from "react";
+import { View, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
+
+import AppText from "../../../utils/AppText";
+import { Button, Icon } from "@rneui/base";
+import { height, width } from "../../../utils/utils";
+
+import { getUsertoken } from "../../../utils/localStorageUtils";
+import urls from "../../../utils/urls";
+
+const Page1 = ({ step, setStep, interest, setInterest }) => {
+  const [disabled, setDisabled] = useState(true);
+  const [sector, setSector] = useState("");
+  const icons = [
+    ["bank", "material-community"], // 금융
+    ["cellphone-message", "material-community"], // 커뮤니케이션 서비스
+    ["factory", "material-community"], // 산업재
+    ["cart-outline", "material-community"], // 필수소비재
+    ["flash-outline", "material-community"], // 유틸리티
+    ["checkcircle", "antdesign"],
+    ["gas-station", "material-community"], // 에너지
+    ["hospital-box-outline", "material-community"], // 건강관리
+    ["laptop", "material-community"], // IT
+    ["texture", "material-community"], // 소재
+    ["shopping", "material-community"], // 경기관련소비재
+  ];
+
+  useEffect(() => {
+    fetchSector().then((data) => setSector(data));
+  }, []);
+  
+  const fetchSector = async () => {
+    try {
+      const token = await getUsertoken();
+      const response = await fetch(`${urls.springUrl}/api/sector/list`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  };
+
+  const handleNextStep = () => {
+    setStep(step + 1);
+  };
+
+  useEffect(() => {
+    if (interest === "") setDisabled(true);
+    else setDisabled(false);
+  }, [interest]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <AppText style={styles.titleText}>
+          분야를 한 가지 선택해주세요.
+        </AppText>
+      </View>
+      <View style={styles.sectorContainer}>
+        <ScrollView persistentScrollbar={true}>
+          {Object.entries(sector).map(([code, name], index) => (
+            <TouchableOpacity
+              key={code}
+              style={styles.sectorContent}
+              onPress={() => setInterest(code)}
+            >
+              <Icon
+                name={icons[index][0]}
+                type={icons[index][1]}
+                color={interest === code ? "#97f697" : "#808080"}
+                style={{ marginRight: 20 }}
+              />
+              <AppText
+                style={[
+                  {
+                    fontSize: 27,
+                    fontWeight: "bold",
+                    color: interest === code ? "#97f697" : "#f0f0f0",
+                  },
+                ]}
+              >
+                {name}
+              </AppText>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </View>
+      <View style={styles.nextButtonContainer}>
+        <Button
+          buttonStyle={styles.nextButton}
+          title="다음"
+          onPress={handleNextStep}
+          disabled={disabled}
+        />
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "stretch",
+  },
+  textContainer: {
+    paddingHorizontal: 10,
+    paddingBottom: 20,
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  sectorContainer: {
+    flex: 1,
+    alignItems: "stretch",
+    backgroundColor: "#333",
+    paddingHorizontal: width * 15,
+    paddingTop: height * 5,
+  },
+  sectorContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingBottom: 7,
+    marginTop: 7,
+    borderBottomColor: "#434343",
+    borderBottomWidth: 1,
+  },
+  nextButton: {
+    backgroundColor: "#6262e8",
+    borderRadius: 10,
+    height: height * 50,
+  },
+  nextButtonContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: height * 5,
+    backgroundColor: "#333",
+  },
+  safeinfoText: {
+    fontSize: 11,
+    marginBottom: 3,
+    color: "#999",
+  },
+});
+export default Page1;
--- Components/pages/portfolio/AddStockInManual.js
+++ Components/pages/portfolio/AddStockInManual.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
+import { Divider } from "@rneui/base";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { usePortfolio } from "../../utils/PortfolioContext";
+import AppText from "../../utils/AppText";
+
+import ManualAddSelect from "../make/manual/ManualAddSelect";
 import ManualPage1 from "../make/manual/ManualPage1";
 import ManualPage2 from "../make/manual/ManualPage2";
 import ManualAddPage3 from "../make/manual/ManualAddPage3";
-import { Button, Divider } from "@rneui/base";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { usePortfolio } from "../../utils/PortfolioContext";
+import Page1 from "../make/manual/Page1";
+import Page2 from "../make/manual/Page2";
 
 const AddStockInManual = ({ route, navigation }) => {
   const pfId = route.params.pfId;
   const [stockList, setStockList] = useState([]);
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(0);  
+  const [interest, setInterest] = useState("");
   const [disabled, setDisabled] = useState(false);
   const { loadData } = usePortfolio();
 
@@ -19,18 +25,18 @@ const AddStockInManual = ({ route, navigation }) => {
   };
 
   useEffect(() => {
-    if (step === 1) {
+    if (step === 1 || step === 3) {
       if (stockList.length === 0) setDisabled(true);
       else setDisabled(false);
     }
-    if (step === 2) {
+    if (step === 4) {
       const result = stockList.filter(
         (stock) => stock.currentPrice === 0 || stock.quantity === 0
       );
       if (result.length) setDisabled(true);
       else setDisabled(false);
     }
-    if (step === 4) {
+    if (step === 6) {
       loadData();
       navigation.popToTop();
       navigation.navigate("ViewPortfolio");
@@ -39,6 +45,13 @@ const AddStockInManual = ({ route, navigation }) => {
 
   const renderManualStep = () => {
     switch (step) {
+      case 0:
+        return (
+          <ManualAddSelect
+            step={step}
+            setStep={setStep}
+          />
+        );
       case 1:
         return (
           <ManualPage1
@@ -50,6 +63,25 @@ const AddStockInManual = ({ route, navigation }) => {
         );
       case 2:
         return (
+          <Page1
+            step={step}
+            setStep={setStep}
+            interest={interest}
+            setInterest={setInterest}
+          />       
+        );
+      case 3:
+        return (
+          <Page2
+            step={step}
+            setStep={setStep}
+            interest={interest}
+            stockList={stockList}
+            setStockList={setStockList}
+          />
+        );
+      case 4:
+        return (
           <ManualPage2
             step={step}
             setStep={setStep}
@@ -57,7 +89,7 @@ const AddStockInManual = ({ route, navigation }) => {
             setStockList={setStockList}
           />
         );
-      case 3:
+      case 5:
         return <ManualAddPage3 stockList={stockList} pfId={pfId} />;
     }
   };
